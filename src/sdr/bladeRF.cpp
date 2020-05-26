2edd229d47d1a5bc7c70b84dececbeecca8b37c2
paddlesteamer <umuter@protonmail.com>
2020-05-26 15:19:46 +0300

    basic run() implementation with a lot of TODO

--- src/sdr/bladeRF.cpp
+++ src/sdr/bladeRF.cpp
@@ -38,7 +38,24 @@ BladeRF::BladeRF(uint64_t frequency) {
 }
 
 void BladeRF::run() {
+    // TODO: add error handling
+    bladerf_enable_module(this->device, BladeRF::CHANNEL, true);
 
+    // TODO:
+    // - check if this is fast enough (maybe create another thread to read samples?)
+    // - test with different configuration and buffer sizes 
+    // - add exit condition
+    uint16_t samples[2 * BladeRF::BUFFER_SIZE];
+    while (true) {
+        // TODO: choose a reasonable timeout and move it to class constants
+        bladerf_sync_rx(this->device, samples, BladeRF::BUFFER_SIZE, NULL, 50);
+
+        for (int i = 0; i < 2 * BladeRF::BUFFER_SIZE; i += 2) {
+            std::complex sample(samples[i+1], samples[i]);
+
+
+        }
+    }
 }
 
 uint64_t BladeRF::getHardwareFrequency() {
