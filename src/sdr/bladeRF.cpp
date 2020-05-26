1f7c4dd1358c5dadd370b1784471dd64e9f3e997
paddlesteamer <umuter@protonmail.com>
2020-05-26 14:36:32 +0300

    BladeRFException && bladeRF initializations

--- src/sdr/bladeRF.cpp
+++ src/sdr/bladeRF.cpp
@@ -3,10 +3,38 @@
 #include "bladeRF.h"
 #include "sdr.h"
 
+// BladeRF implementations
 BladeRF::BladeRF(uint64_t frequency) {
     int status = bladerf_open(&(this->device), "");
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
 
-    
+    status = bladerf_set_frequency(this->device, BladeRF::CHANNEL, frequency);
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
+
+    status = bladerf_set_bandwidth(this->device, BladeRF::CHANNEL, BladeRF::BANDWIDTH, NULL);
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
+
+    status = bladerf_set_sample_rate(this->device, BladeRF::CHANNEL, BladeRF::SAMPLERATE, NULL);
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
+
+    status = bladerf_set_gain(this->device, BladeRF::CHANNEL, BladeRF::GAIN);
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
+
+    status = bladerf_sync_config(this->device, BLADERF_RX_X1, BLADERF_FORMAT_SC16_Q11,
+        BladeRF::NUM_BUFFERS, BladeRF::BUFFER_SIZE, BladeRF::NUM_TRANSFERS, BladeRF::STREAM_TIMEOUT);
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
 }
 
 void BladeRF::run() {
@@ -14,9 +42,30 @@ void BladeRF::run() {
 }
 
 uint64_t BladeRF::getHardwareFrequency() {
-    return 0;
+    uint64_t frequency = 0;
+
+    int status = bladerf_get_frequency(this->device, BladeRF::CHANNEL, &frequency);
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
+
+    return frequency;
 }
 
 uint32_t BladeRF:: getSampleRate() {
-    return 0;
+    uint32_t samplerate = 0;
+
+    int status = bladerf_get_sample_rate(this->device, BladeRF::CHANNEL, &samplerate);
+    if (status != 0) {
+        throw new BladeRFException(status);
+    }
+
+    return samplerate;
+}
+
+// BladeRFException implementations
+BladeRFException::BladeRFException(int status): status(status) {}
+
+const char* BladeRFException::what() const throw() {
+    return bladerf_strerror(this->status);
 }
--- src/sdr/bladeRF.h
+++ src/sdr/bladeRF.h
@@ -1,6 +1,7 @@
 #ifndef _BLADERF_H_
 #define _BLADERF_H_
 
+#include <exception>
 #include <libbladeRF.h>
 
 #include "sdr.h"
@@ -8,7 +9,6 @@
 class BladeRF: public SDR {
     public:
         BladeRF(uint64_t frequency);
-
         
         uint64_t getHardwareFrequency();
         uint32_t getSampleRate();
@@ -19,10 +19,25 @@ class BladeRF: public SDR {
     private:
         static const bladerf_bandwidth BANDWIDTH    = 1000000; // 1M 
         static const bladerf_sample_rate SAMPLERATE = 5000000; // 5M
+        static const bladerf_channel CHANNEL        = BLADERF_CHANNEL_RX(0); // RX0
         static const bladerf_gain GAIN              = 0; // no gain
 
-        struct bladerf *device;
+        static const uint NUM_BUFFERS    = 16;
+        static const uint BUFFER_SIZE    = 8192;
+        static const uint NUM_TRANSFERS  = 8;
+        static const uint STREAM_TIMEOUT = 1000;
 
+        struct bladerf *device;
+};
+
+class BladeRFException: public std::exception {
+    public:
+        BladeRFException(int status);
+        
+        virtual const char* what() const throw();
+    
+    private:
+        int status;
 };
 
 #endif
